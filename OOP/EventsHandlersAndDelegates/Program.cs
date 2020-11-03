dc56a9f71ed2e5310191dd46953625bc3abd9906
Spencer Felton <SpencerEFelton@gmail.com>
2020-11-03 22:11:45 +0000

    Event Prep

---
+++ OOP/EventsHandlersAndDelegates/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsHandlersAndDelegates {
+    class Program {
+
+        //prep for tomorrow's work
+        static void Main(string[] args) {
+        }
+    }
+}
