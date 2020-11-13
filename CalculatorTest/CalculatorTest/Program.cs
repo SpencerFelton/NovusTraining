ec1146848288de7c56ed59ef9395784113dd2218
Spencer Felton <SpencerEFelton@gmail.com>
2020-11-13 16:37:49 +0000

    More calculator unit tests

--- CalculatorTest/CalculatorTest/Program.cs
+++ CalculatorTest/CalculatorTest/Program.cs
@@ -16,11 +16,16 @@ namespace CalculatorTest {
             int b = 7;
             int expectedAns = 12;
             int wrongAns = 89;
+            int nextSumA = 10;
+            int nextSumB = 14;
+            int nextExpectedAns = 24;
             //Act
             int ans = calc.Add(a, b);
+            int nextAns = calc.Add(nextSumA, nextSumB);
             //Assert
             //Assert.AreEqual(wrongAns, ans); //False answer to show Assert can fail
             Assert.AreEqual(expectedAns, ans);
+            Assert.AreEqual(nextExpectedAns, nextAns);
         }
         [Test]
         public void testSubtract() {
@@ -30,11 +35,16 @@ namespace CalculatorTest {
             int b = 12;
             int expectedAns = -2; // check negatives
             int wrongAns = 2;
+            int nextSubA = 20;
+            int nextSubB = 24;
+            int nextExpectedAns = -4;
             //Act
             int ans = calc.Subtract(a, b);
+            int nextAns = calc.Subtract(nextSubA, nextSubB);
             //Assert
             //Assert.AreEqual(wrongAns, ans); //False answer to show Assert can fail
             Assert.AreEqual(expectedAns, ans);
+            Assert.AreEqual(nextExpectedAns, nextAns);
         }
         [Test]
         public void testMultiply() {
@@ -44,11 +54,16 @@ namespace CalculatorTest {
             int b = 9;
             int expectedAns = 108; //
             int wrongAns = 22;
+            int nextMulA = 15;
+            int nextMulB = 14;
+            int nextExptectedAns = 210;
             //Act
             int ans = calc.Multiply(a, b);
+            int nextAns = calc.Multiply(nextMulA, nextMulB);
             //Assert
             //Assert.AreEqual(wrongAns, ans); //False answer to show Assert can fail
             Assert.AreEqual(expectedAns, ans);
+            Assert.AreEqual(nextExptectedAns, nextAns);
         }
         [Test]
         public void testDivide() {
@@ -58,11 +73,16 @@ namespace CalculatorTest {
             int b = 3;
             int expectedAns = 33; // check negatives
             int wrongAns = 45;
+            int nextDivA = 27;
+            int nextDivB = 9;
+            int nextExpectedAns = 3;
             //Act
             int ans = calc.Divide(a, b);
+            int nextAns = calc.Divide(nextDivA, nextDivB);
             //Assert
             //Assert.AreEqual(wrongAns, ans); //False answer to show Assert can fail
             Assert.AreEqual(expectedAns, ans);
+            Assert.AreEqual(nextExpectedAns, nextAns);
         }
     }
 }
