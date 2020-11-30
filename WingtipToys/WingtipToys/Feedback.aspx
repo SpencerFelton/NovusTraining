4d3fb0de235e1ea52c5db23750ad83ca8bfa4d1a
Spencer Felton <SpencerEFelton@gmail.com>
2020-11-30 20:00:14 +0000

    Added feedback form to WingTipToys ASP.NET project

---
+++ WingtipToys/WingtipToys/Feedback.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Feedback.aspx.cs" Inherits="WingtipToys.Feedback" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Feedback Form</h3>
+    <p>Please fill in the details below to give us feedback about your experience.</p>
+    <asp:Label ID="emailAddressLabel" runat="server" Text="Email Address"></asp:Label>   <asp:TextBox ID="emailAddress" placeholder="Someone1234@gmail.com" runat="server"></asp:TextBox>
+    <br /><br /><br />
+    <asp:DropDownList ID="categoryDropdown" OnSelectedIndexChanged="categoryDropdown_SelectedIndexChanged" AutoPostback="true" runat="server"></asp:DropDownList>
+    <asp:DropDownList ID="productDropdown" AutoPostback="true" runat="server"></asp:DropDownList>
+    <br /><br />
+    <asp:TextBox ID="feedbackTextBox" placeholder="Please type your feedback here!" TextMode="MultiLine" Rows=10 runat="server"></asp:TextBox>
+    <br />
+    <asp:Button ID="feedbackSubmit" runat="server" OnClick="feedbackSubmit_Click" AutoPostBack="true" Text="Submit"/>
+</asp:Content>
---
+++ WingtipToys/WingtipToys/Feedback.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WingtipToys {
+    public partial class Feedback : Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            if (!IsPostBack)
+            {
+                fillDropdown();
+            }
+
+        }
+
+        protected void feedbackSubmit_Click(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                string productName = productDropdown.Text;
+                int productID = -1;
+                string s = ConfigurationManager.ConnectionStrings["WingTipToys"].ConnectionString;
+                SqlConnection con = new SqlConnection(s);
+                string sqlString = "select ProductID from Products where ProductName=@productName";
+                SqlCommand cmd = new SqlCommand(sqlString, con);
+                cmd.Parameters.AddWithValue("@productName", productName);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                System.Diagnostics.Debug.WriteLine(dr.ToString());
+                if (dr.Read())
+                {
+                    productID = dr.GetInt32(0);
+                }
+                dr.Close();
+
+                int FeedbackID = 0;
+                string sqlString2 = "select MAX(FeedbackID) from Feedback";
+                SqlCommand cmd2 = new SqlCommand(sqlString2, con);
+                SqlDataReader dr2 = cmd2.ExecuteReader();
+                if (dr2.Read())
+                {
+                    FeedbackID = dr2.GetInt32(0);
+                }
+                dr2.Close();
+
+                string sqlString3 = "insert into Feedback values(@feedbackID, @productID, @productName, @emailAddress, @feedback)";
+                SqlCommand cmd3 = new SqlCommand(sqlString3, con);
+                cmd3.Parameters.AddWithValue("@feedbackID", FeedbackID+1);
+                cmd3.Parameters.AddWithValue("@productID", productID);
+                cmd3.Parameters.AddWithValue("@productName", productName);
+                cmd3.Parameters.AddWithValue("@emailAddress", emailAddress.Text); // Unsafe - not protected against SQL injection
+                cmd3.Parameters.AddWithValue("@feedback", feedbackTextBox.Text);
+                cmd3.ExecuteNonQuery();
+
+                con.Close();
+
+
+            }
+        }
+
+        protected void categoryDropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                productDropdown.Items.Clear();
+                String txtValue = categoryDropdown.SelectedValue.ToString();
+                string s = ConfigurationManager.ConnectionStrings["WingTipToys"].ConnectionString;
+                SqlConnection con = new SqlConnection(s);
+                int categoryID = stringtoID(txtValue);
+                if(categoryID == 6)
+                {
+                    productDropdown.Items.Add("General Feedback");
+                }
+                System.Diagnostics.Debug.WriteLine("CAATEGORY ID: " + categoryID);
+                string sqlString = "select ProductName from Products where CategoryID=@categoryID";
+                SqlCommand cmd = new SqlCommand(sqlString, con);
+                cmd.Parameters.AddWithValue("@categoryID", categoryID);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read() == true)
+                {
+                    productDropdown.Items.Add(dr["ProductName"].ToString());
+                }
+                dr.Close();
+                con.Close();
+            }
+        }
+        protected void fillDropdown()
+        {
+            categoryDropdown.Items.Add("Cars");
+            categoryDropdown.Items.Add("Planes");
+            categoryDropdown.Items.Add("Trucks");
+            categoryDropdown.Items.Add("Ships");
+            categoryDropdown.Items.Add("Rockets");
+            categoryDropdown.Items.Add("General Feedback");
+        }
+
+        protected int stringtoID(string s)
+        {
+            switch (s)
+            {
+                case "Cars":
+                    return 1;
+                case "Planes":
+                    return 2;
+                case "Trucks":
+                    return 3;
+                case "Ships":
+                    return 4;
+                case "Rockets":
+                    return 5;
+                case "General Feedback":
+                default:
+                    return 6;
+            } 
+        }
+    }
+}
