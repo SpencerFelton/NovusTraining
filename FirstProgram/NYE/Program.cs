70658205b03993fb2488a8a81ea555a01200156a
Spencer Felton <SpencerEFelton@gmail.com>
2020-12-31 23:32:21 +0000

    New Years Eve Program
    Happy New Years!

---
+++ FirstProgram/NYE/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = new DateTime();
+            DateTime currentDate = DateTime.Now;
+            _ = new DateTime();
+            string date = "1/1/2021 0:00:01 AM";
+            DateTime newYears = DateTime.Parse(date);
+            if (currentDate.Date.DayOfYear == newYears.DayOfYear)
+            {
+                Console.WriteLine("Happy New Year!");
+            }
+            else
+            {
+                Console.WriteLine("Come back on New Years Day!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
