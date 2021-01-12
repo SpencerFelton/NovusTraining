f990b262a5dcebf7fe4bca56fd13091e649b698a
Spencer Felton <SpencerEFelton@gmail.com>
2021-01-12 15:50:48 +0000

    Current Individual Contribution to Group Project

---
+++ StockValueProject/StockTrackingApp/StockTrackingApp/Producer/ProducerAPICaller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class ProducerAPICaller
+    {
+        private static HttpClient _httpClient = new HttpClient();
+        public ProducerAPICaller()
+        {
+            _httpClient.BaseAddress = new Uri("https://localhost:44399/"); //base address
+            _httpClient.Timeout = new TimeSpan(0, 0, 30); // 30s timeout limit
+        }
+        
+        public async Task<string> SendStockValue(string a)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, _httpClient.BaseAddress+$"api/SendValue/{a}");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> TestConn()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, _httpClient.BaseAddress + $"api");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
