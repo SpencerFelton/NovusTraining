8677d66cdc32955d8ee38a9ba301962fd21c1e6e
Spencer Felton <SpencerEFelton@gmail.com>
2021-01-13 16:59:27 +0000

    Added Base Web Form

---
+++ StockTrackingApp/Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProducerAPICaller prod = new ProducerAPICaller();
+            while (true)
+            {
+                Console.WriteLine("Please enter a value to send");
+                string value = Console.ReadLine().Trim();
+                if (value.ToLower().Equals("q"))
+                {
+                    break;
+                }
+                Task.Run(() =>SendAPIRequest(prod, value));
+            }
+        }
+
+        static async Task SendAPIRequest(ProducerAPICaller prod, string val)
+        {
+            await prod.SendStockValue(val);
+        }
+
+        static async Task<string> CheckConn(ProducerAPICaller prod)
+        {
+            return await prod.TestConn();
+        }
+    }
+}
---
+++ StockTrackingApp/ProducerWebForm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProducerWebForm.Global" Language="C#" %>
