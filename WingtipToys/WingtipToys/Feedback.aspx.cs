c15ec9c7de56e01e46b86a163fd040db89574363
Spencer Felton <SpencerEFelton@gmail.com>
2021-01-24 20:54:57 +0000

    Installed LGTM on the stocktrackingapp repo

--- WingtipToys/WingtipToys/Feedback.aspx.cs
+++ WingtipToys/WingtipToys/Feedback.aspx.cs
@@ -12,7 +12,7 @@ namespace WingtipToys {
         protected void Page_Load(object sender, EventArgs e) {
             if (!IsPostBack)
             {
-                FillDropdown();
+                FillDropdown(); // load the dropdowns on the page when it loads
             }
 
         }
