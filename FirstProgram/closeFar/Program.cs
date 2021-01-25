5fecc7fc90a37933b9c912e9aaba0ca29fafa273
Spencer Felton <SpencerEFelton@gmail.com>
2021-01-25 21:40:23 +0000

    Codingbat LuckySum challenge

---
+++ FirstProgram/closeFar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace closeFar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int luckySum(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            if(a == 13)
+            {
+                sum -= a;
+            }
+            if(b == 13)
+            {
+                sum -= 13;
+            }
+            if(c == 13)
+            {
+                sum -= 13;
+            }
+
+            return sum;
+        } 
+    }
+}
