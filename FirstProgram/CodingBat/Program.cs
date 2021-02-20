311983c2fdc1852c4a8e7bcd4d6472cd9ec6d090
Spencer Felton <SpencerEFelton@gmail.com>
2021-02-20 22:48:08 +0000

    Weekendly Commit

--- FirstProgram/CodingBat/Program.cs
+++ FirstProgram/CodingBat/Program.cs
@@ -52,7 +52,7 @@ namespace CodingBat {
         }
 
         public static String missingChar(String str, int n) { // returns string str after removing char at index n
-            StringBuilder sb = new StringBuilder(str);
+            StringBuilder sb = new StringBuilder(str); // create new stringbuilder
             sb.Remove(n, 1); // remove char at index n
             Console.WriteLine(sb.ToString());
             Console.ReadLine();
