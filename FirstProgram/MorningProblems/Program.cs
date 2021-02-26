e26e855de86b90c3cc06b78874954309b792d2c6
Spencer Felton <SpencerEFelton@gmail.com>
2021-02-26 22:16:03 +0000

    weekend prep

--- FirstProgram/MorningProblems/Program.cs
+++ FirstProgram/MorningProblems/Program.cs
@@ -6,32 +6,39 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MorningProblems {
-    class Program {
-        static void Main(string[] args) {
+    class Program
+    {
+        static void Main(string[] args)
+        {
             Console.WriteLine(longestSequence("1,2,1,1,0,3,1,0,0,2,4,1,0,0,0,0,2,1,0,3,1,0,0,0,6,1,3,0,0,0"));
             Console.ReadLine();
 
-            
+
         }
 
 
-    public static int longestSequence(String sales) {
+        public static int longestSequence(String sales) // return the longest string of the same number of a string of numbers separated by commas
+        { 
             String splitSales = sales.Replace(",", "");
             int days = 0;
             int longestDays = 0;
-            for(int i = 0; i < splitSales.Length; i++) {
-                if(splitSales[i].ToString().Equals("0")) {
+            for (int i = 0; i < splitSales.Length; i++)
+            {
+                if (splitSales[i].ToString().Equals("0"))
+                {
                     days += 1;
-                    if(days > longestDays) {
+                    if (days > longestDays)
+                    {
                         longestDays = days;
                     }
                 }
-                else {
+                else
+                {
                     days = 0;
                 }
             }
             return longestDays;
         }
 
-    
+    }
 }
